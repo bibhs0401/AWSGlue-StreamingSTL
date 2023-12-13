dd6e19d9292d4158b5180f387952a2993bc87bb5
bibhs0401 <bibhushabibhs@gmail.com>
2023-12-13 12:10:27 +0545

    kinesis connector trial

--- script-glue-streaming-etl.py
+++ script-glue-streaming-etl.py
@@ -1,3 +1,4 @@
+!pip install kinesis
 import sys
 import datetime
 import base64
@@ -13,12 +14,14 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue import DynamicFrame
 
-args = getResolvedOptions(sys.argv, \
-                            ['JOB_NAME', \
-                            'aws_region', \
-                            'checkpoint_location', \
-                            'dynamodb_sink_table', \
-                            'dynamodb_static_table'])
+# Get arguments
+args = getResolvedOptions(sys.argv, [
+    'JOB_NAME',
+    'aws_region',
+    'checkpoint_location',
+    'dynamodb_sink_table',
+    'dynamodb_static_table'
+])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -28,68 +31,49 @@ job.init(args['JOB_NAME'], args)
 
 # Read parameters
 checkpoint_location = args['checkpoint_location']
-aws_region = args['us-west-2']
-
-# DynamoDB config
+aws_region = args['aws_region']
 dynamodb_sink_table = args['dynamodb_sink_table']
 dynamodb_static_table = args['dynamodb_static_table']
 
 def write_to_dynamodb(row):
-    '''
-    Add row to DynamoDB.
-    '''
-    dynamodb = boto3.resource('dynamodb', region_name=aws_region)
-    start = str(row['window'].start)
-    end = str(row['window'].end)
-    dynamodb.Table(dynamodb_sink_table).put_item(
-      Item = { 'userid': row['userid'], \
-                'channelid':row['channelid'], \
-                'genre': str(row['genre']), \
-                'lastactive': str(row['lastactive']), \
-                'title': str(row['title']), \
-                'watchfrequency': row['watchfrequency'], \
-                'etags': row['etags'] })
+    dynamodb = glueContext.create_dynamic_frame.fromDF(row, glueContext, "write_to_dynamodb")
+    dynamodb.toDF().write \
+        .format("dynamodb") \
+        .option("tableName", dynamodb_sink_table) \
+        .option("region", aws_region) \
+        .mode("Append") \
+        .save()
 
-#
-
-dynamodb_dynamic_frame = glueContext.create_dynamic_frame.from_options( \
-    "dynamodb", \
+# Read from DynamoDB as a DataFrame
+dynamodb_dynamic_frame = glueContext.create_dynamic_frame.from_options(
+    connection_type="dynamodb",
     connection_options={
-    "dynamodb.input.tableName": dynamodb_static_table,
-    "dynamodb.throughput.read.percent": "1.5"
-  }
+        "dynamodb.input.tableName": dynamodb_static_table,
+        "dynamodb.throughput.read.percent": "1.5"
+    }
 )
 
 dynamodb_lookup_df = dynamodb_dynamic_frame.toDF().cache()
 
 # Read from Kinesis Data Stream
 netflix_data = spark.readStream \
-                    .format("kinesis") \
-                    .option("streamName","bibhusha-demo-datastream") \
-                    .option("endpointUrl", "https://kinesis.us-west-2.amazonaws.com") \
-                    .option("startingPosition", "TRIM_HORIZON") \
-                    .load()
+    .format("kinesis") \
+    .option("streamName", "bibhusha-demo-datastream") \
+    .option("region", "us-west-2") \
+    .load()
 
+# Stream static join, ETL to augment with 'impression' column
+netflix_df = netflix_data.withColumn('impression',
+    when(netflix_data['watchfrequency'] < 3, "neutral")
+    .when(((netflix_data['watchfrequency'] >= 3) & (netflix_data['watchfrequency'] <= 10)), "like")
+    .otherwise("favorite")
+)
 
-# Stream static join, ETL to augment with favorite
-netflix_df = netflix_data.withColumn('impression', when( \
-    ((netflix_data.watchfrequency < 3)), "neutral") \
-    .when(((netflix_data.watchfrequency >= 3) |
-    (netflix_data.watchfrequency <= 10)), "like") \
-    .otherwise("favorite"))
-
-
-netflix_df.printSchema()
-
-# Drop some values
 netflix_transformed_df = netflix_df.drop('etags')
 
-netflix_transformed_df.printSchema()
-
-# Write to DynamoDB sink
-netflix_query = netflix_transformed_df \
-    .writeStream \
-    .foreach(write_to_dynamodb) \
+# Write to DynamoDB sink using foreachBatch
+netflix_query = netflix_transformed_df.writeStream \
+    .foreachBatch(lambda batch_df, batch_id: batch_df.foreachPartition(write_to_dynamodb)) \
     .outputMode("update") \
     .option("checkpointLocation", checkpoint_location) \
     .start()
